12ea029f524117a3755d4bffc404feeef9dc4ea1
minhanhng-cd <minhanh@coderschool.vn>
2020-06-24 12:55:37 +0700

    Add Q&A link

--- uku.py
+++ uku.py
@@ -14,11 +14,12 @@ client = discord.Client()
 # ----COMMANDS----
 
 # Command: Send important links
-def important_links():
+def links():
     response = """Here are some useful links! Meowww
-    - Learning Portal: http://learning.coderschool.vn/courses/ftmle_philippines/unit/1#!module
-    - Google Drive: https://drive.google.com/drive/folders/1rRz0c0gwAAdkQIz7eRlJoMbbCeDADSTE
-    - Performance Dashboard: https://datastudio.google.com/u/0/reporting/1EmxAbhqjUuG8JaXZ9w-UCWEioW2U-5sU/page/IkS6
+    - Learning Portal: <http://learning.coderschool.vn/courses/ftmle_philippines/unit/1#!module>
+    - Google Drive: <https://drive.google.com/drive/folders/1rRz0c0gwAAdkQIz7eRlJoMbbCeDADSTE>
+    - Performance Dashboard: <https://datastudio.google.com/u/0/reporting/1EmxAbhqjUuG8JaXZ9w-UCWEioW2U-5sU/page/IkS6>
+    - Q&A: <https://hackmd.io/Lf4aEx-nSYWfHGmlGafGig?both>
     """
     return response
 
@@ -27,9 +28,6 @@ def cheers():
     with open('cheers.txt','r') as f:
         cheers = [line for line in f]
     return random.choice(cheers)
-
-commands = {'links': 'important_links()',
-            'cheers': 'cheers()'}
 # ----------------
 
 @client.event
@@ -52,7 +50,7 @@ async def on_message(message):
     # Check if bot name is mentioned
     if 'Uku' in [u.name for u in message.mentions]:
         command = message.content.split()[-1]
-        response = eval(commands[command])
+        response = eval(command + '()')
         await message.channel.send(response)
 
 client.run(TOKEN)
