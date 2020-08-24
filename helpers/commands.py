8de48cedf7d9a88f7c28d48d4e3141e38d45ecb1
minhanhng-cd <minhanh@coderschool.vn>
2020-08-24 11:37:37 +0000

    Disable tf

--- helpers/commands.py
+++ helpers/commands.py
@@ -3,35 +3,35 @@ import random
 from datetime import datetime 
 from helpers import gsheet
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
 import requests
 
 # model = tf.keras.models.load_model('models/catdog.h5')
 
 # Preprocess an image
-def preprocess_image(image):
-    image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [224, 224])
-    image /= 255.0  # normalize to [0,1] range
+#def preprocess_image(image):
+ #   image = tf.image.decode_jpeg(image, channels=3)
+  #  image = tf.image.resize(image, [224, 224])
+   # image /= 255.0  # normalize to [0,1] range
 
-    return image
+    #return image
 
 # Read the image from path and preprocess
-def load_and_preprocess_image(path):
-    image = tf.io.read_file(path)
-    return preprocess_image(image)
+#def load_and_preprocess_image(path):
+ #   image = tf.io.read_file(path)
+  #  return preprocess_image(image)
 
 # Predict & classify image
-def classify(model, image_path):
+#def classify(model, image_path):
 
-    preprocessed_imgage = load_and_preprocess_image(image_path)
-    preprocessed_imgage = tf.reshape(preprocessed_imgage, (1,224 ,224 ,3))
+ #   preprocessed_imgage = load_and_preprocess_image(image_path)
+  #  preprocessed_imgage = tf.reshape(preprocessed_imgage, (1,224 ,224 ,3))
 
-    prob = model.predict(preprocessed_imgage)
-    label = "Cat" if prob >= 0.5 else "Dog"
-    classified_prob = prob if prob >= 0.5 else 1 - prob
+   # prob = model.predict(preprocessed_imgage)
+    #label = "Cat" if prob >= 0.5 else "Dog"
+    #classified_prob = prob if prob >= 0.5 else 1 - prob
     
-    return label, classified_prob
+   # return label, classified_prob
 
 # Command: Show available commands
 def help(message, channel):
